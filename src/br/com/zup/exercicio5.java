a9710c84522143a32a0c1d4d55eaa639c8a70edc
Larissa Novais <larinovais54@gmail.com>
2021-08-24 17:34:54 -0300

    primeiro commit exercicio5

---
+++ src/br/com/zup/exercicio5.java
@@ -0,0 +1,13 @@
+package br.com.zup;
+
+public class exercicio5 {
+    public static void main(String[] args) {
+        int temperaturaFahrenheit = 50;
+        int soma = (50 - 32) * 5/9;
+
+        int Resultado = 10;
+
+        System.out.println("Se a temperatura em fahrenheit for igaul a " + temperaturaFahrenheit);
+        System.out.println(" o valor em Graus Celsius será igual a "+ soma + " graus");
+    }
+}
