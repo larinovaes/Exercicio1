94aacde888e31deb7e0d5311dd7732a001a317ca
Larissa Novais <larinovais54@gmail.com>
2021-08-24 16:52:12 -0300

    primeiro commit do exercicio 3

---
+++ src/br/com/zup/exercicio3.java
@@ -0,0 +1,10 @@
+package br.com.zup;
+
+public class exercicio3 {
+    public static void main(String[] args) {
+        int centimetros = 4;
+        int soma = centimetros * 100;
+
+        System.out.println("Na converção a soma de " + centimetros + " metros mais 100 será igual a "+ soma + " centímetros");
+    }
+}
