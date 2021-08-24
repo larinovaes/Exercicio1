e0c06c520fa244f6e94de0ef1cbc8eef339ff750
Larissa Novais <larinovais54@gmail.com>
2021-08-24 16:21:44 -0300

    segundo commit do exercicio 2

--- src/br/com/zup/exercicio2.java
+++ src/br/com/zup/exercicio2.java
@@ -1,2 +1,21 @@
-package br.com.zup;public class exercicio2 {
+package br.com.zup;
+
+public class exercicio2 {
+    public static void main(String[] args) {
+
+        //Definindo notas
+        int primeiraNota = 10;
+        float segundaNota = 8.7F;
+        float terceiraNota = 7.6F;
+        int quartaNota = 10;
+
+        //respondendo a pergunta
+        float soma = primeiraNota + segundaNota + terceiraNota + quartaNota;
+        float resultadoFinal = soma /5;
+
+        System.out.println("As suas notas durante o bimestre foram " + primeiraNota);
+        System.out.println(segundaNota);
+        System.out.println(terceiraNota);
+        System.out.println(quartaNota +" e sua media final será " + resultadoFinal);
+    }
 }
